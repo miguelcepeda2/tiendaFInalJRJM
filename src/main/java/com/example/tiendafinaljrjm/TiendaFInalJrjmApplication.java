c1af94e441ef3b12f38cebdd8b03bce64fb668a9
Miguel <alumno@gmail.com>
2023-05-02 13:14:59 +0200

    Initial commit

---
+++ src/main/java/com/example/tiendafinaljrjm/TiendaFInalJrjmApplication.java
@@ -0,0 +1,13 @@
+package com.example.tiendafinaljrjm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TiendaFInalJrjmApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TiendaFInalJrjmApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/tiendafinaljrjm/TiendaFInalJrjmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.tiendafinaljrjm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TiendaFInalJrjmApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
